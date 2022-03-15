3baa5ad381cba859787726ddad5b34cef3e200fa
AndersJakobsen <ande12n3@easv365.dk>
2022-03-15 12:52:03 +0100

    Gui + Controller basic setup

---
+++ eventassignment7/src/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static FXMLLoader fxmlLoaderMain;
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        fxmlLoaderMain = new FXMLLoader(getClass().getResource("./gui/view/login.fxml"));
+        primaryStage.centerOnScreen();
+        Scene scene = new Scene(fxmlLoaderMain.load());
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Event Manager");
+        primaryStage.show();
+    }
+}
---
+++ eventassignment7/src/gui/Controller/LoginController.java
@@ -0,0 +1,54 @@
+package gui.Controller;
+
+import gui.util.SceneSwapper;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+
+public class LoginController implements Initializable {
+
+    public TextField lblUsername;
+    public TextField lblPassword;
+
+
+    public void onLoginBtn(ActionEvent actionEvent) throws IOException {
+
+        if (lblUsername.getText().toLowerCase(Locale.ROOT).equals("admin") && lblPassword.getText().toLowerCase(Locale.ROOT).equals("admin")) {
+
+            SceneSwapper sceneSwapper = new SceneSwapper();
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setResizable(false);
+            sceneSwapper.sceneSwitch(stage, "AdminScreen.fxml");
+        }
+
+        if(lblUsername.getText().toLowerCase(Locale.ROOT).equals("Coordinator") && lblPassword.getText().toLowerCase(Locale.ROOT).equals("Coordinator")){
+
+            SceneSwapper sceneSwapper = new SceneSwapper();
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setResizable(false);
+            sceneSwapper.sceneSwitch(stage, "CoordinatorScreen.fxml");
+        }
+
+        if(lblUsername.getText().toLowerCase(Locale.ROOT).equals("User") && lblPassword.getText().toLowerCase(Locale.ROOT).equals("User")){
+
+            SceneSwapper sceneSwapper = new SceneSwapper();
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setResizable(false);
+            sceneSwapper.sceneSwitch(stage, "UserScreen.fxml");
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+}

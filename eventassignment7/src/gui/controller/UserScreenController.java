98531408040f8cb86b8ee89bc98ee3048912d3ae
Math376i <91528384+Math376i@users.noreply.github.com>
2022-03-21 11:15:31 +0100

    Logout btn

--- eventassignment7/src/gui/controller/UserScreenController.java
+++ eventassignment7/src/gui/controller/UserScreenController.java
@@ -1,6 +1,20 @@
 package gui.controller;
 
+import gui.util.SceneSwapper;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class UserScreenController {
 
+
+    public void onBtnLogOut(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        SceneSwapper sceneSwapper = new SceneSwapper();
+        sceneSwapper.sceneSwitch(new Stage(), "Login.fxml");
+        stage.close();
+    }
     
 }

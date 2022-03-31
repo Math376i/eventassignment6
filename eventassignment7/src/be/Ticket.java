edc69d8bfa8f7852b3ed182066e47695972c94c9
magn20 <magnus.overgaard@live.dk>
2022-03-31 22:31:01 +0200

    be "Ticket" class

--- eventassignment7/src/be/Ticket.java
+++ eventassignment7/src/be/Ticket.java
@@ -1,24 +1,67 @@
 package be;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Ticket {
-    private String EventName;
-    private String Address;
+
+
+    private final StringProperty eventName = new SimpleStringProperty();
+    private final StringProperty guestName = new SimpleStringProperty();
+    private final StringProperty address = new SimpleStringProperty();
+    private final StringProperty startTime = new SimpleStringProperty();
+
+    public Ticket(String eventName, String guestName, String address, String startTime){
 
 
 
-    public String getAddress() {
-        return Address;
+    }
+
+    public void setEventName(String eventName){
+        this.eventName.set(eventName);
     }
 
     public String getEventName() {
-        return EventName;
+        return eventName.get();
     }
 
-    public void setEventName(String eventName) {
-        EventName = eventName;
+    public StringProperty eventNameProperty() {
+        return eventName;
     }
 
-    public void setAddress(String address) {
-        Address = address;
+    public void setGuestName( String guestName){
+        this.guestName.set(guestName);
+    }
+
+    public String getGuestName() {
+        return guestName.get();
+    }
+
+    public StringProperty guestNameProperty() {
+        return guestName;
+    }
+
+    public void setAddress(String address){
+        this.address.set(address);
+    }
+
+    public String getAddress() {
+        return address.get();
+    }
+
+    public StringProperty addressProperty() {
+        return address;
+    }
+
+    public void setStartTime(String startTime){
+        this.startTime.set(startTime);
+    }
+
+    public String getStartTime() {
+        return startTime.get();
+    }
+
+    public StringProperty startTimeProperty() {
+        return startTime;
     }
 }

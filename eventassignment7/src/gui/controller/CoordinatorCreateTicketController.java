e9ee18ec7c1a3b8592509ee970cefb20e4f41054
Math376i <91528384+Math376i@users.noreply.github.com>
2022-04-05 09:03:02 +0200

    Back button from ticketcontroller to controllerscreen

--- eventassignment7/src/gui/controller/CoordinatorCreateTicketController.java
+++ eventassignment7/src/gui/controller/CoordinatorCreateTicketController.java
@@ -1,4 +1,20 @@
 package gui.controller;
 
+import gui.util.SceneSwapper;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CoordinatorCreateTicketController {
+    private SceneSwapper sceneSwapper;
+
+    public void onBack(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        SceneSwapper sceneSwapper = new SceneSwapper();
+        sceneSwapper.sceneSwitch(new Stage(), "CoordinatorScreen.fxml");
+        stage.close();
+    }
+
 }

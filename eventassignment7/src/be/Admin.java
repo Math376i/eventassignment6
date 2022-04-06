77d64dc6b97e880e7e73165b987e0a58bbc88337
Math376i <91528384+Math376i@users.noreply.github.com>
2022-04-06 10:12:44 +0200

    Admin update propperty

--- eventassignment7/src/be/Admin.java
+++ eventassignment7/src/be/Admin.java
@@ -1,27 +1,42 @@
 package be;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Admin {
 
-   private String username;
-   private String password;
 
-   public Admin(String username, String password) {;
+   private SimpleStringProperty username = new SimpleStringProperty();
+   private SimpleStringProperty password = new SimpleStringProperty();
+
+   public Admin( String username, String password,int AdminCreator){
+
+      setUsername(username);
+      setPassword(password);
+      setAdminCreater(AdminCreator);
    }
+
+   private void setAdminCreater(int adminCreator) {
+   }
+
 
    public String getUsername() {
-      return username;
+      return username.get();
    }
 
-   public void setUsername(String username) {
-      this.username = username;
+   public final void setUsername(String username){
+      this.username.set(username);
    }
 
    public String getPassword() {
-      return password;
+      return password.get();
    }
 
-   public void setPassword(String password) {
-      this.password = password;
+   public final void setPassword(String password){
+      this.password.set(password);
    }
 
 }
+
